07b3cce244a095599d0244449e4e4554584c2ad7
delfinak37 <delfinak37@mail.ru>
2023-05-16 18:20:58 +0300

    dddd

--- 32.cpp
+++ 32.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int temp = 1;
@@ -31,19 +32,21 @@ class Point
 		void show(vector<Point>& worker)
 		{
 			int n = 0;
-			cout << "\n================================\n";
+			cout << "\n	 |WORKERS DATA|\n"
+				<< "********************************\n";
 			while(n < worker.size())
 			{
-				cout << worker[n].name << " " << worker[n].locate << " " << worker[n].a
-					<< " " << worker[n].b << " " << worker[n].c << endl;
+				cout << worker[n].name << " " << worker[n].locate 
+					<< "  " << worker[n].a << " " << worker[n].b 
+					<< " " << worker[n].c << "  " << worker[n].salary 
+					<< "$" << endl;
 				n++;
 			}
-			cout << "================================\n\n";
+			cout << "********************************\n\n\n";
 		}	
 		
 		void push(Point p, vector<Point>& worker)
 		{
-			
 			ofstream fout;
 			fout.open("text32-2.txt");
 			if (!fout) cout << "File not founded!!";
@@ -54,8 +57,8 @@ class Point
 			
 			while(n < worker.size())
 			{	
-				fout << worker[n].name << " " << worker[n].locate << " -- A=" 
-					<< worker[n].a << " B=" << worker[n].b << " C=" 
+				fout << worker[n].num << ") " <<worker[n].name << " " << worker[n].locate << " -- " 
+					<< worker[n].a << " " << worker[n].b << " " 
 					<< worker[n].c << " --  " << worker[n].salary << "$" << endl;
 				
 				if(worker[n].locate == "A1")
@@ -80,7 +83,7 @@ class Point
 			fout << endl << "Workshop salary:\nA1 -- " << a1/d1 
 				<< "$\nA2 -- " << a2/d2 << "$\nA3 -- " << a3/d3 << "$";
 			fout.close();
-			
+			cout << "Saved successfully!\n\n";
 		}
 		
 		void del(vector<Point>& worker) 
@@ -98,17 +101,19 @@ class Point
 				}
 				it++;
 			}
-			cout << endl;
+			if(found == true)
+				cout << "|Worker successfully deleted|\n\n";
+			else
+				cout << "|Worker not founded|\n\n";
 		}
 			
 		void enter(vector<Point>& worker)
 		{
 			Point t;
-			int a,b,c; a = b = c = 0;
 			int num = temp;	temp++;
 			string name,locate; name = locate = "";
 			
-			cout << "Enter candidate information:\n\nEnter name: ";
+			cout << "Enter candidate information:\nName: ";
 				cin >> t.name;
 				
 				int k = 1;
@@ -125,26 +130,71 @@ class Point
 						}
 					k = 0;
 				}
-				cout << "\n";
 				
-			cout << "Enter number workshop: ";
-				cin >> t.locate; cout << "\n";
-			cout << "Enter quantity products -A- : ";
-				cin >> t.a; cout << "\n";
-			cout << "Enter quantity products -B- : ";
-				cin >> t.b; cout << "\n";
-			cout << "Enter quantity products -C- : ";
-				cin >> t.c; cout << "\n";
+			cout << "Number workshop: ";
+				cin >> t.locate;
+			cout << "Quantity products -A- : ";
+				cin >> t.a;
+			cout << "Quantity products -B- : ";
+				cin >> t.b;
+			cout << "Quantity products -C- : ";
+				cin >> t.c;	cout << "\n";
 				t.salary = t.salary = t.a*5 + t.b*15 + t.c*20;
 			worker.push_back(t);
 		}
 		
 		void sorting(vector<Point>& worker)
 		{
-			sort(worker.begin(), worker.end(), [](Point& e1, Point& e2) {return e1.name < e2.name; });
+			int n = -1;
+			while(n)
+			{
+				switch(n)
+				{
+					case 1:
+						sort(worker.begin(),worker.end(),[](Point n1, Point n2) {return n1.name < n2.name;});
+						n = 0;
+						break;
+					case 2:
+						sort(worker.begin(),worker.end(),[](Point n1, Point n2) {return n1.locate < n2.locate;});
+						n = 0;
+						break;
+					case 3:
+						sort(worker.begin(),worker.end(),[](Point n1, Point n2) {return n1.a < n2.a;});
+						n = 0;
+						break;
+					case 4:
+						sort(worker.begin(),worker.end(),[](Point n1, Point n2) {return n1.b < n2.b;});
+						n = 0;
+						break;
+					case 5:
+						sort(worker.begin(),worker.end(),[](Point n1, Point n2) {return n1.c < n2.a;});
+						n = 0;
+						break;		
+					case 6:
+						sort(worker.begin(),worker.end(),[](Point n1, Point n2) {return n1.salary < n2.salary;});
+						n = 0;
+						break;
+					case 7:
+						sort(worker.begin(),worker.end(),[](Point n1, Point n2) {return n1.num < n2.num;});
+						n = 0;
+						break;
+					default:
+						{
+							cout << "Sort by:\n1 - Name\n2 - Workshop\n3 - Products -A- "
+								<<"\n4 - Products -B- \n5 - Products -C- \n6 - Salary"
+								<<"\n7 - Worker number\nChoosen: ";
+							cin >> n; cout << "\n";
+							if(n < 0 or n > 7)
+							{
+								cout << "\nDenied! Enter choosen again\n";
+								cin >> n;
+							}
+							break;	
+						}
+				}
+			}
 		}
 };
-
 
 
 
@@ -177,14 +227,19 @@ int main()
 				break;
 			case 5:
 				p.sorting(worker);
+				p.show(worker);
 				n = -1;
 				break;
 			default:
 				{
-					cout << "1 - Add worker\n2 - Delete worker\n3 " 
-						<< "- Save data of workers\n4 - See data of workers\n0 - Exit\nChosen: ";
+					cout << "	   |GENERAL|"
+						<<"\n================================\n"
+						<<"1 - Add worker\n2 - Delete worker\n3 " 
+						<< "- Save data of workers\n4 - Show data of"
+						<< " workers\n5 - Sorting data\n0 - Exit\n"
+						<< "================================\nChosen: ";
 					cin >> n; cout << "\n";
-							if(n < 0 or n > 4)
+							if(n < 0 or n > 5)
 							{
 								cout << "\nDenied! Enter choosen again\n";
 								cin >> n;
